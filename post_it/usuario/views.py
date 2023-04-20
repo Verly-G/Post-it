4238079ea5b0a038846fd51c7252af510a36f6ac
Vitor <88908365+VitorAssuncaoSantos@users.noreply.github.com>
2023-04-20 18:16:03 -0300

    20.04.2023.1.VA
    Precisamos corrigir a validação do forms.

--- post_it/usuario/views.py
+++ post_it/usuario/views.py
@@ -6,15 +6,17 @@ def index (request):
     return render(request, 'index.html')
 
 def criar_post_it(request):
+
     if request.method == 'POST':
         form = PostItForm(request.POST)
+        print(form.is_valid)
         if form.is_valid():
             descricao = form.cleaned_data['descricao']
             categoria = form.cleaned_data['categoria']
-            postit = Postit(descricao=descricao, categoria =categoria)
             nova_categoria = Categoria(nome=categoria)
-            postit.save() # Salvando no banco de dados
             nova_categoria.save()
+            postit = Postit(descricao=descricao)
+            postit.save() # Salvando no banco de dados
             return redirect('index.html') # redirecionar para a view desejada após o sucesso do formulário
     else:
         form = PostItForm()
